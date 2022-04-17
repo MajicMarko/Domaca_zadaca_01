93f1369474df105f06565f55a80e152ee668957b
Marko Majić <mmajic@racunarstvo.hr>
2022-04-17 16:01:04 +0200

    first commit

--- SpaDomacaZadaca01/Source.cpp
+++ SpaDomacaZadaca01/Source.cpp
@@ -1,10 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include"Cvijet.h"
+
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 600), "Hello, SFML world!");
 	window.setFramerateLimit(60);
-	//Cvijet cvijet(&window);
+	sf::Texture texture;
+	texture.loadFromFile("nebo.jpg");
+	sf::Sprite sprite(texture);
+	sprite.setTextureRect(sf::IntRect(0, 0, 800, 600));
+	Cvijet cvijet(&window);
+
+
 
 	while (window.isOpen())
 	{
@@ -16,9 +24,10 @@ int main()
 		}
 
 		window.clear();
-		//cvijet.draw();
+		window.draw(sprite);
+		cvijet.crtaj();
 		window.display();
-	}
 
+	}
 	return 0;
 }
